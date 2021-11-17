961edb1072b1ed151210efe764b6469c8b0a4f80
Noyemi Ohanyan <nomiohanyan@gmail.com>
2021-11-16 20:05:05 -0500

    fixed issue, changed handlebars extension

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-const path = require("path");
 const express = require("express");
-const session = require("express-session");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+// require 'path' for static folder below
+const path = require("path");
+
+// add hbs helper functions
+const helpers = require("./utils/helpers");
+
+// set up Handlebars.js as your app's template engine of choice & add helper functions:
 const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+
+// set up express-session & sequelize store:
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
 const sess = {
-  secret: "Super secret secret",
+  secret: "Super cool secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -21,17 +30,21 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Static assets folder
+app.use(express.static(path.join(__dirname, "public")));
+app.use(require("./controllers/home-routes"));
+app.set("views", "./views");
+app.set("views", path.join(__dirname, "views"));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+// turn on routes
+app.use(routes);
 
-app.use(require("./controllers/"));
-
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
